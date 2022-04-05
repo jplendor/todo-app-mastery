cd2ad43ddec44960714eec2e2daed0cf0de0668c
Junha <prodopark15@gmail.com>
2022-04-05 12:18:56 +0900

    refact : updateToDo func

--- todo.js
+++ todo.js
@@ -1,92 +1,46 @@
-const $toDoForm = document.querySelector('.todo-form');
 const $pendingList = document.querySelector('.pending-list');
 const $finishedList = document.querySelector('.finished-list');
 
 const PENDING = 'pending';
 const FINISHED = 'finished';
 
-let pending_global = [];
-let finished_global = [];
+let pendingData = [];
+let finishedData = [];
 
-const filteredTodo = (listName, li) => {
-  let toDos =
-    listName === PENDING //
-      ? pending_global
-      : finished_global;
-  const newToDo = toDos.filter((toDo) => toDo.id !== li.id);
-  return newToDo;
+const saveToDo = (listName, newList) => {
+  localStorage.setItem(listName, JSON.stringify(newList));
 };
 
 const removeDOM = (listName, li) => {
-  if (listName === PENDING) {
-    $pendingList.removeChild(li);
-  } else {
-    $finishedList.removeChild(li);
-  }
+  listName === PENDING
+    ? $pendingList.removeChild(li)
+    : $finishedList.removeChild(li);
 };
 
 const deleteToDo = (e) => {
   const li = e.target.parentNode;
   const listName = li.parentNode.className.split('-')[0];
-  const newToDo = filteredTodo(listName, li);
 
   removeDOM(listName, li);
-  console.log(newToDo);
-  saveToDo(listName, newToDo);
-
-  listName === PENDING
-    ? (pending_global = newToDo)
-    : (finished_global = newToDo);
+  updateToDo('DEL', listName, li.id, null);
 };
 
 const moveToDo = (e) => {
   const isChecked = e.target.checked;
   const li = e.target.parentNode;
   const text = e.target.nextSibling.textContent;
-
   const listName = isChecked ? PENDING : FINISHED;
-  const newToDo = filteredTodo(listName, li);
 
   removeDOM(listName, li);
-  saveToDo(listName, newToDo);
+  updateToDo('DEL', listName, li.id, null);
 
   if (listName === PENDING) {
-    pending_global = newToDo;
     paintToDo(FINISHED, li.id, text);
+    updateToDo('ADD', FINISHED, li.id, text);
   } else {
-    finished_global = newToDo;
     paintToDo(PENDING, li.id, text);
+    updateToDo('ADD', PENDING, li.id, text);
   }
-};
-
-const saveToDo = (listName, newList) => {
-  localStorage.setItem(listName, JSON.stringify(newList));
-};
-
-const paintToDo = (listName, id, text) => {
-  const li = ToDoList(listName, text);
-  let toDoTemps = [];
-  let toDoList = [];
-
-  if (listName === PENDING) {
-    toDoTemps = pending_global;
-    toDoList = $pendingList;
-  } else {
-    toDoTemps = finished_global;
-    toDoList = $finishedList;
-  }
-
-  const newId = id === null ? Math.random().toString(16).slice(2, 10) : id;
-  li.id = newId;
-  toDoList.appendChild(li);
-
-  const newToDo = {
-    id: newId,
-    text,
-  };
-
-  toDoTemps.push(newToDo);
-  saveToDo(listName, toDoTemps);
 };
 
 const ToDoList = (listName, text) => {
@@ -95,15 +49,16 @@ const ToDoList = (listName, text) => {
   const checkBox = document.createElement('input');
   const textNode = document.createTextNode(text);
 
+  checkBox.classList.add('todo-checkbox');
   checkBox.setAttribute('type', 'checkbox');
-  checkBox.classList.add('btn-check');
   checkBox.addEventListener('change', moveToDo);
-  listName === FINISHED && checkBox.setAttribute('checked', true);
-  listName === FINISHED && li.classList.add('completed');
 
-  delBtn.setAttribute('class', 'btn-del');
+  delBtn.classList.add('todo-delete');
   delBtn.innerText = 'DEL';
   delBtn.addEventListener('click', deleteToDo);
+
+  listName === FINISHED && checkBox.setAttribute('checked', true);
+  listName === FINISHED && li.classList.add('completed');
 
   li.appendChild(checkBox);
   li.appendChild(textNode);
@@ -112,30 +67,69 @@ const ToDoList = (listName, text) => {
   return li;
 };
 
+const updateToDo = (type, listName, id, text) => {
+  const prevToDos = listName === PENDING ? pendingData : finishedData;
+  let newToDos = '';
+
+  switch (type) {
+    case 'ADD':
+      const newToDo = { id, text };
+      newToDos = [...prevToDos, newToDo];
+      break;
+    case 'DEL':
+      newToDos = prevToDos.filter((toDo) => toDo.id !== id);
+      break;
+    default:
+      break;
+  }
+
+  saveToDo(listName, newToDos);
+  listName === PENDING //
+    ? (pendingData = newToDos)
+    : (finishedData = newToDos);
+};
+
+const paintToDo = (listName, id, text) => {
+  const li = ToDoList(listName, text);
+  li.id = id;
+
+  if (listName === PENDING) {
+    $pendingList.appendChild(li);
+  } else {
+    $finishedList.appendChild(li);
+  }
+};
+
 const loadToDo = () => {
-  const loadedPending = localStorage.getItem('pending');
-  const loadedFinished = localStorage.getItem('finished');
+  const pendingData = localStorage.getItem(PENDING);
+  const finishedData = localStorage.getItem(FINISHED);
 
-  if (loadedPending) {
-    const parsedPending = JSON.parse(loadedPending);
-    parsedPending.forEach((toDo) => paintToDo(PENDING, toDo.id, toDo.text));
-  }
+  pendingData &&
+    JSON.parse(pendingData).forEach((todo) => {
+      paintToDo(PENDING, todo.id, todo.text);
+      updateToDo(PENDING, todo.id, todo.text);
+    });
 
-  if (loadedFinished) {
-    const parsedFinished = JSON.parse(loadedFinished);
-    parsedFinished.forEach((toDo) => paintToDo(FINISHED, toDo.id, toDo.text));
-  }
+  finishedData &&
+    JSON.parse(pendingData).forEach((todo) => {
+      paintToDo(FINISHED, todo.id, todo.text);
+      updateToDo(FINISHED, todo.id, todo.text);
+    });
 };
 
-const toDoInit = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const value = e.target['todo-input'].value;
-    e.target['todo-input'].value = '';
-    paintToDo(PENDING, null, value);
-  };
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const text = e.target['todo-input'].value;
+  const newId = Math.random().toString(16).slice(2, 5);
+
+  e.target['todo-input'].value = '';
+  paintToDo(PENDING, newId, text);
+  updateToDo('ADD', PENDING, newId, text);
+};
+
+const init = () => {
+  document.querySelector('.todo-form').addEventListener('submit', handleSubmit);
   loadToDo();
-  $toDoForm.addEventListener('submit', handleSubmit);
 };
 
-toDoInit();
+init();
